1a211d305620035a2d4973a51e7215235381e5fa
mrdude2478 <mrdude2478@gmail.com>
2026-02-12 11:11:41 +0000

    Remove jailbreak option from menu
    Commented out the jailbreak menu option as it is unnecessary.

--- src/download0/main-menu.ts
+++ src/download0/main-menu.ts
@@ -45,7 +45,8 @@ import { fn, BigInt } from 'download0/types'
 
   const menuOptions = [
     { label: lang.payloadMenu, script: 'payload_host.js', imgKey: 'payloadMenu' },
-    { label: lang.jailbreak, script: 'loader.js', imgKey: 'jailbreak' },
+    // Jailbreak button is not required, since we are already jailbroken if we see this menu....
+    //{ label: lang.jailbreak, script: 'loader.js', imgKey: 'jailbreak' },
     { label: lang.config, script: 'config_ui.js', imgKey: 'config' }
   ]
 
